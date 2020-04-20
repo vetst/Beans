efa654571544da64a23ceb7c28823fa663ecc7ce
vetst <vetstv@gmail.com>
2020-04-20 17:59:44 +0300

    first commit

---
+++ 2.1.3. spring-beans/src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,27 @@
+package app.model;
+
+import app.model.Animal;
+import app.model.Timer;
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component()
+public class AnimalsCage {
+
+    @Autowired
+    @Qualifier("dog")
+    private Animal animal;
+
+    @Autowired
+    private Timer timer;
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(timer.getTime());
+        System.out.println("________________________");
+    }
+}
